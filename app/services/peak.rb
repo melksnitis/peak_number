8f307e2970c4b4bc9669662d381f795379e50c7b
Mikus <mikus.elksnitis@gmail.com>
2018-03-14 22:16:03 +0200

    Compleated version and rspec tests

---
+++ app/services/peak.rb
@@ -0,0 +1,70 @@
+class Peak
+  # num and action are not instance variables.
+  # let me know how you were going to use these.
+  attr_accessor :array, :split_posibilitys , :split_options_count
+
+
+  def initialize(array)
+    @array = array
+    @split_posibilitys = []
+    @split_options_count = 0
+  end
+
+
+  def find_peak_options
+    options = split_options
+    splited_arrays = create_splited_arrays(options)
+
+    splited_arrays.each_with_index do |splited_array, index|
+      @split_posibilitys << options[index] if find_peak(splited_array)
+    end
+
+    @split_options_count = @split_posibilitys.count
+  end
+
+  def random_array(size)
+    Array.new(size) { rand(0...1000000000) }
+  end
+
+
+
+  def create_splited_arrays(options, split = [])
+    options.each do |item|
+      split << @array.each_slice(item).to_a
+    end
+    split
+  end
+
+  def find_peak(arr)
+    arr.each_with_index do |i, index|
+      exist = false
+      element_count = i.length
+
+      for f in 0..element_count-1
+
+        pos = element_count * index + f
+        exist = true  if peak_exist?(pos)
+      end
+
+      return false if !exist
+    end
+    true
+  end
+
+  def peak_exist?(pos)
+    return false if pos >= @array.length-1
+    @array[pos - 1] < @array[pos] && @array[pos] > @array[pos + 1]
+  end
+
+  def split_options(n_array = [])
+    ar_l = @array.length
+
+    for i in 2..ar_l
+      next if (@array.length % i) != 0
+      n_array << i
+    end
+
+    return n_array
+  end
+
+end
---
+++ spec/services/peak_spec.rb
@@ -0,0 +1,27 @@
+require './app/services/peak'
+describe Peak do
+
+    let(:peak) { Peak.new([1, 2, 2, 2, 6, 2, 2, 6, 2, 2, 6, 2, 5, 8, 5]) }
+
+
+
+    it "splits array in right options" do
+      expect(peak.split_options).to eq [3, 5, 15]
+    end
+
+    it "can find a peak" do
+      splited = [[1, 2, 6, 2, 6], [2, 2, 6, 2, 2], [6, 2, 5, 8, 5]]
+      expect(peak.find_peak(splited)).to eq true
+    end
+
+    it "can't find a peak" do
+      splited = [[1, 2, 2],[ 2, 6, 2], [2, 6, 2], [2, 6, 2], [5, 8, 5] ]
+      expect(peak.find_peak(splited)).to eq false
+    end
+
+    it "array is splited in right portions" do
+      peak.find_peak_options
+      expect(peak.split_posibilitys).to eq [5, 15]
+    end
+
+end
